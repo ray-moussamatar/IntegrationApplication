46a31fd58191f1408e90f7e58f0ae7633e01df82
Moussa Matar <moussa.matar@getray.com>
2026-02-10 16:30:16 +0200

    testing flows

--- src/main/java/com/example/IntegrationApplication/IntegrationApplication.java
+++ src/main/java/com/example/IntegrationApplication/IntegrationApplication.java
@@ -1,13 +1,53 @@
 package com.example.IntegrationApplication;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.GenericHandler;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
+
+import java.time.Instant;
 
 @SpringBootApplication
 public class IntegrationApplication {
 
-	public static void main(String[] args) {
+	 static void main(String[] args) {
 		SpringApplication.run(IntegrationApplication.class, args);
+	}
+
+	@Bean
+	MessageChannel atob(){
+		return MessageChannels.direct().getObject();
+	}
+
+
+	@Bean
+	MessageChannel atoc(){
+		return new DirectChannel();
+//		return MessageChannels.direct().getObject();
+	}
+	@Bean
+	IntegrationFlow Flow() {
+		return IntegrationFlow
+				.from((MessageSource<String>) () -> MessageBuilder.withPayload("hello World @ " + Instant.now()+ "!").build(), poller -> poller.poller(pm -> pm.fixedRate(1000)))
+
+				.channel(atob())
+				.get();
+	}
+
+	@Bean
+	IntegrationFlow flow1 (){
+		return IntegrationFlow
+				.from(atob())
+				.handle((GenericHandler<String>) (payload, _) -> {
+					System.out.println("the payload is " + payload);
+					return null;
+				})
+				.get();
 	}
 
 }
